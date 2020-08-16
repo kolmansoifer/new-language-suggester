9046358c1bcb50af07953f53723f5f2ca715f6b6
kolmansoifer <kolmansoifer@gmail.com>
2020-08-16 12:24:12 -0700

    Add message to message class to result

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,23 @@ $(document).ready(function() {
     const interest = parseInt($("select#interest").val());
     const os = parseInt($("select#os").val());
 
-    $(".message").show();
+
+  $(".message").show();
+      
+      
+      
+  if (skill === 1) {
+    $("#python").show();
+  } else if (skill === 2) {
+    $("#javascript").show();
+  } else if (skill === 3) {
+    $("#hoon").show();
+  } 
+
+
+
+
     
-    
-    
-    if (skill === 1) {
-      $("#python").show();
-    } else if (skill === 2) {
-      $("#javascript").show();
-    } else if (skill === 3) {
-      $("#hoon").show();
-    } 
     
     
     console.log(interest)
