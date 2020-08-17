7963a24b697bf254932e686786f04e42ab7ca44e
kolmansoifer <kolmansoifer@gmail.com>
2020-08-16 17:51:12 -0700

    Adds more functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,23 @@ $(document).ready(function() {
       
       
       
-  if (skill === 1) {
+  if (skill === 1 && os === 9) {
     $("#python").show();
-  } else if (skill === 2) {
+  } else if (skill === 2 && os === 8) {
     $("#javascript").show();
-  } else if (skill === 3) {
+  } else if (skill === 3 && os === 7) {
+    $("#hoon").show();
+  } else if (skill === 1 && os === 8) {
+    $("#python").show();
+  } else if (skill === 2 && os === 7) {
+    $("#javascript").show();
+  } else if (skill === 3 && os === 9) {
+    $("#hoon").show();
+  } else if (skill === 1 && os === 7) {
+    $("#python").show();
+  } else if (skill === 2 && os === 9) {
+    $("#javascript").show();
+  } else if (skill === 3 && os === 8) {
     $("#hoon").show();
   } 
 
