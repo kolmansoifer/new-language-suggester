4ec955236d155fd9d05c1286515ec6b7e2b620ed
kolmansoifer <kolmansoifer@gmail.com>
2020-08-31 14:22:34 -0700

    add any new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
-$(document).ready(function() {
-  $("form#language").submit(function(event) {
+$(document).ready(function () {
+  $("form#language").submit(function (event) {
 
     const skill = parseInt($("select#skill").val());
     const interest = parseInt($("select#interest").val());
     const os = parseInt($("select#os").val());
 
-  $(".message").show();     
-      
-  if ((skill === 1 && os === 9) || (skill === 1 && os === 7) || (skill === 1 && os === 9)) {
-    $("#python").show();
-  } else if ((skill === 2 && interest === 4) || (skill === 2 && interest === 5) || (skill === 2 && interest === 6)) {
-    $("#python").show();
-  } else {
-    $("#hoon").show();
-  } 
+    $(".message").show();
 
-  event.preventDefault();  
-  
+    if ((skill === 1 && os === 9) || (skill === 1 && os === 7) || (skill === 1 && os === 9)) {
+      $("#hoon").hide();
+      $("#javascript").hide();
+      $("#python").show();
+    } else if ((skill === 2 && interest === 4) || (skill === 2 && interest === 5) || (skill === 2 && interest === 6)) {
+      $("#hoon").hide();
+      $('#python').hide();
+      $("#javascript").show();
+    } else {
+      $("#python").hide();
+      $("#javascript").hide();
+      $("#hoon").show();
+    }
+
+    event.preventDefault();
+
   });
 });
-  
